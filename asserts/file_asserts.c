9be906c992ed21f94482a30a04a754c28bf22e68
LemurPwned <jakub.mojsiejuk@gmail.com>
2018-11-08 23:57:10 +0100

    adding file asserts

---
+++ asserts/file_asserts.c
@@ -0,0 +1,79 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../utils/utils.h"
+#include "../structure_defs/job_def.h"
+
+void assert_file_or_directory_existence(char **filename)
+{
+    if (access(filename, R_OK | W_OK))
+    {
+        perror("Either the file does not exist or you don't have W/R permisssions");
+        exit(-1);
+    }
+}
+
+void test_regex()
+{
+    char *ex_regex = "sbatch: Submitted batch job 123456789";
+    int result = extract_job_number(ex_regex);
+    assert(result == 123456789);
+}
+
+void test_logging()
+{
+    FILE *fp;
+    // create file
+    char *test_filename = "asserts/assert.log";
+    fp = fopen(test_filename, "w");
+    assert(fp != NULL);
+    write(fp, "TEST", sizeof("TEST"));
+    close(fp);
+    // use it as log file in the append mode
+    fp = fopen(test_filename, "a");
+    assert(fp != NULL);
+    log_to_file(fp, 'I', "THIS IS A TEST MESSAGE");
+    assert_file_or_directory_existence(test_filename);
+    /*
+    TODO:
+        Clean up the file by removing it
+        Check the contents of the file.
+    */
+}
+
+void test_popen()
+{
+#ifdef __unix__
+    /*
+    popen is not defined for all linux-type system
+    thats probs the most reliable check we can have
+    */
+    char output[200]; // 200 should be enough but crash if not
+    FILE *rp;         // file handler is provided but actual pipe is opened in the function
+    pipe_opener(rp, "echo HELLO", output, 0);
+    // make sure the pipe returns proper stuff when not in the silent mode
+    assert(!strcmp(output, "HELLO\n"));
+#else
+    perror("Not an UNIX type system");
+#endif
+}
+
+void test_job_extraction()
+{
+    char job_size[20];
+    FILE *fp;
+    pipe_opener(fp, "cat queue.txt |cut -d ' ' -f 11 | wc -l", job_size, 0);
+    int job_num = strtol(job_size, NULL, 10);
+    JOB_INF jobs[job_num];
+    get_status_jobs(jobs, job_num);
+}
+int main(int argc, char *argv[])
+{
+    test_regex();
+    test_logging();
+    test_popen();
+    test_job_extraction();
+    return 0;
+}
---
+++ asserts/file_asserts.h
@@ -0,0 +1,8 @@
+#if !defined(ASSERT)
+#define ASSERT
+
+void assert_file_or_directory_existence(char **filename);
+
+void test_regex();
+
+#endif
