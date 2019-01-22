728d6da4c43f683af0331b2b84052f20d8d24911
LemurPwned <jakub.mojsiejuk@gmail.com>
2019-01-22 15:33:31 +0100

    adding vasp shell script

---
+++ vasp_init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DONOR_FOLDER=$2
+VASP_ROOT_FOLDER=$1
+
+GRID_SWIFT="."
+
+if [ -d "$VASP_ROOT_FOLDER" ]; then
+    # DIRECTORY EXISTS
+    echo "DIRECTORY $VASP_ROOT_FOLDER exists"
+else 
+    echo "DIRECTORY $VASP_ROOT_FOLDER does not exist... Created"
+    mkdir $VASP_ROOT_FOLDER
+fi
+
+ION_RELAX="$VASP_ROOT_FOLDER/ION_RELAX"
+ELECTRON_RELAX="$VASP_ROOT_FOLDER/ELECTRON_RELAX"
+NON_COLINEAR="$VASP_ROOT_FOLDER/NON_COLINEAR"
+
+shopt -s extglob
+for FOLDER in $ION_RELAX $ELECTRON_RELAX $NON_COLINEAR; do
+    mkdir $FOLDER
+    cp ${DONOR_FOLDER%%+(/)}${DONOR_FOLDER:+/}{INCAR,POSCAR,POTCAR,KPOINTS} $FOLDER
+done 
+
+# for ION RELAX make sure ICHARG is 2 and NSW is 10
+sed -i -r -e 's/ICHARG\s+=\s+[0-9]+/ICHARG = 2/g' -e 's/NSW\s+=\s+[0-9]+/NSW = 10/g' $ION_RELAX/INCAR 
+cp $ION/RELAX/CHARGCAR $ELECTRON_RELAX
+
+# remove ION relaxation for ELECTRON RELAX and start from CHARGCAR
+sed -i -r -e 's/ICHARG\s+=\s+[0-9]+/ICHARG = 1/g' -e 's/NSW\s+=\s+[0-9]+/NSW = 0/g' $ELECTRON_RELAX/INCAR 
+cp $ELECTRON_RELAX/RELAX/CHARGCAR $NON_COLINEAR
+
+# for non-colinear create kpoints path
+sed -i -r -e 's/ICHARG\s+=\s+[0-9]+/ICHARG = 11/g' -e 's/NSW\s+=\s+[0-9]+/NSW = 0/g' $NON_COLINEAR/INCAR 
+python3 $GRID_SWIFT/vasp_kpoints.py -s $NON_COLINEAR -p GHNGP
